d1bc22ca8afdb371f0a18971da151f74224ef049
denisYordanov-eng <denisivov437@gmail.com>
2025-09-15 15:37:48 +0300

    Initial commit

---
+++ CSharp-Calculator-Project/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using CSharp_Calculator_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharp_Calculator_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate (Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0.0m;
+            string operatorSymbol = calculator.Operator;
+
+            if (operatorSymbol == "+")
+            {
+                result = calculator.LeftOperand + calculator.RightOperand;
+            }
+            else if (operatorSymbol == "-")
+            {
+                result = calculator.LeftOperand - calculator.RightOperand;
+            }else if(operatorSymbol == "*")
+            {
+                result = calculator.LeftOperand * calculator.RightOperand;
+            }
+            else if (operatorSymbol == "/")
+            {
+                if (calculator.LeftOperand == 0 || calculator.RightOperand == 0)
+                {
+                    result = 0;
+                }
+                else
+                {
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                }
+            }
+                return result;
+        }
+    }
+}
---
+++ CSharp-Calculator-Project/Models/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharp_Calculator_Project.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        { 
+            this.Result = 0;
+        }
+        [Required]
+        public decimal LeftOperand { get; set; }
+        [Required]
+        public decimal RightOperand { get; set; }
+        [Required]
+        public string Operator { get; set; }
+
+        public decimal Result { get; set; }
+    }
+}
